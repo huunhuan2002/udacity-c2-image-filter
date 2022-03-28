d44d5c8e0da989f19a9e664edc618d125b9829a2
Le Huu Nhuan <huunhuan2002@gmail.com>
2022-03-28 23:04:55 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
-    res.status(4000).send("try GET /filteredimage?image_url={{}}")
+    res.send("try GET /filteredimage?image_url={{}}")
   } );
   
   app.get( "/filteredimage", async ( req, res ) => {
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
-import Jimp = require("jimp");
+import axios from 'axios';
+import Jimp from 'jimp'
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -11,7 +12,12 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const res = await axios({
+        method: 'get',
+        url: inputURL,
+        responseType: 'arraybuffer'
+      })
+      const photo = await Jimp.read(res.data);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
